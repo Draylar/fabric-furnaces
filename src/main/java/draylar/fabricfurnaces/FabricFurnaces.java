e5cc3c86b8cacbcbe643e0cc0e1f1a55e394b75e
Draylar <samuelhimes@gmail.com>
2020-09-03 22:02:17 -0500

    1.16.2, jankson -> gson

--- src/main/java/draylar/fabricfurnaces/FabricFurnaces.java
+++ src/main/java/draylar/fabricfurnaces/FabricFurnaces.java
@@ -4,19 +4,18 @@ import draylar.fabricfurnaces.config.FabricFurnacesConfig;
 import draylar.fabricfurnaces.registry.Blocks;
 import draylar.fabricfurnaces.registry.Entities;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
 import me.sargunvohra.mcmods.autoconfig1u.serializer.JanksonConfigSerializer;
-import me.sargunvohra.mcmods.autoconfig1u.shadowed.blue.endless.jankson.Jankson;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class FabricFurnaces implements ModInitializer {
 
-	public static final FabricFurnacesConfig CONFIG = AutoConfig.register(FabricFurnacesConfig.class, JanksonConfigSerializer::new).getConfig();
+	public static final FabricFurnacesConfig CONFIG = AutoConfig.register(FabricFurnacesConfig.class, GsonConfigSerializer::new).getConfig();
 	public static final ItemGroup GROUP = FabricItemGroupBuilder.build(id("group"), () -> new ItemStack(Registry.ITEM.get(id("fabric_furnace"))));
 
 	@Override
